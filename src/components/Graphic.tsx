340fe88adf4e8e7582666a0c72aebf9d0e9ad6b7
jennifertakagi <jennitakagi@gmail.com>
2021-07-01 09:49:09 -0300

    feat: add Graphic component and style

---
+++ src/components/Graphic.tsx
@@ -0,0 +1,68 @@
+import dynamic from 'next/dynamic';
+import { Box, Text, theme } from '@chakra-ui/react';
+
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      '2021-06-27T00:00:00.00Z',
+      '2021-06-28T00:00:00.00Z',
+      '2021-06-29T00:00:00.00Z',
+      '2021-06-30T00:00:00.00Z',
+      '2021-07-01T00:00:00.00Z',
+      '2021-07-02T00:00:00.00Z',
+      '2021-07-03T00:00:00.00Z',
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
+};
+
+const series = [{ name: 'series1', data: [31, 61, 18, 68, 100, 160, 299] }];
+
+interface GraphicProps {
+  title: string;
+  type: string;
+}
+
+export default function Graphic({ title, type }: GraphicProps): JSX.Element {
+  return (
+    <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+      <Text fontSize="lg">{title}</Text>
+      <Chart options={options} series={series} type={type} height={160} />
+    </Box>
+  );
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,60 +1,8 @@
-import dynamic from 'next/dynamic';
-import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react';
+import { Flex, SimpleGrid } from '@chakra-ui/react';
 
+import Graphic from '../components/Graphic';
 import Header from '../components/Header';
 import SideBar from '../components/SideBar';
-
-const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
-
-const options = {
-  chart: {
-    toolbar: {
-      show: false,
-    },
-    zoom: {
-      enabled: false,
-    },
-    foreColor: theme.colors.gray[500],
-  },
-  grid: {
-    show: false,
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  tooltip: {
-    enabled: false,
-  },
-  xaxis: {
-    type: 'datetime',
-    axisBorder: {
-      color: theme.colors.gray[600],
-    },
-    axisTicks: {
-      color: theme.colors.gray[600],
-    },
-    categories: [
-      '2021-06-27T00:00:00.00Z',
-      '2021-06-28T00:00:00.00Z',
-      '2021-06-29T00:00:00.00Z',
-      '2021-06-30T00:00:00.00Z',
-      '2021-07-01T00:00:00.00Z',
-      '2021-07-02T00:00:00.00Z',
-      '2021-07-03T00:00:00.00Z',
-    ],
-  },
-  fill: {
-    opacity: 0.3,
-    type: 'gradient',
-    gradient: {
-      shade: 'dark',
-      opacityFrom: 0.7,
-      opacityTo: 0.3,
-    },
-  },
-};
-
-const series = [{ name: 'series1', data: [31, 61, 18, 68, 100, 160, 299] }];
 
 export default function Dashboard(): JSX.Element {
   return (
@@ -65,15 +13,8 @@ export default function Dashboard(): JSX.Element {
         <SideBar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px">
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg">Weekly Subscription</Text>
-            <Chart options={options} series={series} type="area" height={160} />
-          </Box>
-
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg">Conversions</Text>
-            <Chart options={options} series={series} type="area" height={160} />
-          </Box>
+          <Graphic title="Weekly Subscription" type="area" />
+          <Graphic title="Conversions" type="area" />
         </SimpleGrid>
       </Flex>
     </Flex>
