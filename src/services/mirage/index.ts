bb330a531c84c3a44da11102fe5b245a88ebdc8b
jennifertakagi <jennitakagi@gmail.com>
2021-08-29 09:42:53 -0300

    feat: configure pagination on Mirage.js

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,5 +1,5 @@
 import faker from 'faker';
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 
 type User = {
   name: string;
@@ -28,14 +28,27 @@ export function makeServer(): any {
     },
 
     seeds(server) {
-      server.createList('user', 10);
+      server.createList('user', 200);
     },
 
     routes() {
       this.namespace = 'api';
       this.timing = 750;
 
-      this.get('/users');
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+        const total = schema.all('user').length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd,
+        );
+
+        return new Response(200, { 'x-total-count': String(total) }, { users });
+      });
       this.post('/users');
 
       this.namespace = '';
