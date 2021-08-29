319b082f1c3ade87376f32b668100a31f8e74c35
jennifertakagi <jennitakagi@gmail.com>
2021-08-29 08:53:57 -0300

    feat: get data using ReactQuery

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from 'react-query';
 import Link from 'next/link';
 import {
   Box,
@@ -6,6 +7,8 @@ import {
   Heading,
   Icon,
   useBreakpointValue,
+  Spinner,
+  Text,
 } from '@chakra-ui/react';
 import { RiAddLine } from 'react-icons/ri';
 
@@ -15,6 +18,25 @@ import SideBar from '../../components/SideBar';
 import Table from '../../components/Table';
 
 export default function UserList(): JSX.Element {
+  const { data, error, isLoading } = useQuery('users', async () => {
+    const response = await fetch('https:localhost:3000/api/users');
+    const data = await response.json();
+    const users = data.users.map(user => {
+      return {
+        createdAt: new Date(user.createdAt).toLocaleDateString('en-US', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }),
+        email: user.email,
+        id: user.id,
+        name: user.name,
+      };
+    });
+
+    return users;
+  });
+
   const isWideVersion = useBreakpointValue({ base: false, lg: true });
 
   return (
@@ -43,9 +65,21 @@ export default function UserList(): JSX.Element {
             </Link>
           </Flex>
 
-          <Table isMobileVersion={isWideVersion} />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Fail on getting user data</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table data={data} isMobileVersion={isWideVersion} />
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
